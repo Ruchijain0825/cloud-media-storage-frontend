7e5714f0644a10e78e7a49e561c14621a56232cc
Ruchi Jian <learnwithfunroo@gmail.com>
2026-09-04 16:23:11 +0530

    made small changes

--- src/app/dashboard/components/FileExplorer/apis/folder.api.js
+++ src/app/dashboard/components/FileExplorer/apis/folder.api.js
@@ -1,4 +1,4 @@
-const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+const API_URL = "https://cloud-media-storage-backend.onrender.com/api";
 
 const getToken = () => localStorage.getItem("accessToken");
 
