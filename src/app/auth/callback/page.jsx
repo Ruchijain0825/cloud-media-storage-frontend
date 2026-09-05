3a21df9d68f76838e05cf60dfedbd6af3e7471d7
Ruchi Jian <learnwithfunroo@gmail.com>
2026-09-05 14:17:41 +0530

    latest changes

--- src/app/auth/callback/page.jsx
+++ src/app/auth/callback/page.jsx
@@ -3,21 +3,21 @@
 import { useEffect } from "react";
 
 export default function CallbackPage() {
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const token = params.get("token");
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const accessToken = params.get("accessToken");
 
-    if (token) {
-      localStorage.setItem("token", token);
-      window.location.href = "/dashboard";
-    } else {
-      window.location.href = "/login";
-    }
-  }, []);
+        if (accessToken) {
+            localStorage.setItem("accessToken", accessToken);
+            window.location.href = "/dashboard";
+        } else {
+            window.location.href = "/login";
+        }
+    }, []);
 
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>Signing you in...</p>
-    </div>
-  );
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <p>Signing you in...</p>
+        </div>
+    );
 }
